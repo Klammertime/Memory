14159e951f270b290014f3288db79b1b39297f9e
“Klammertime” <audrey.ck@gmail.com>
2016-01-11 11:58:36 -0800

    Add script-compiled.js and .babelrc

--- scripts/script-compiled.js
+++ scripts/script-compiled.js
@@ -6,40 +6,32 @@
 
 // The array below contains the images'file names
 // with the corresponding alt attribute
-var sources = [
-  {
-    file: 'robot29.svg',
-    alt: 'robot29'
-  },
-  {
-    file: 'robot30.svg',
-    alt: 'robot30'
-  },
-  {
-    file: 'robot26.svg',
-    alt: 'robot26'
-  },
-  {
-    file: 'robot25.svg',
-    alt: 'robot25'
-  },
-  {
-    file: 'robot22.svg',
-    alt: 'robot22'
-  },
-  {
-    file: 'robot17.svg',
-    alt: 'robot17'
-  },
-  {
-    file: 'robot21.svg',
-    alt: 'robot21'
-  },
-  {
-    file: 'rounded46.svg',
-    alt: 'rounded46'
-  },
-];
+
+var sources = [{
+  file: 'robot29.svg',
+  alt: 'robot29'
+}, {
+  file: 'robot30.svg',
+  alt: 'robot30'
+}, {
+  file: 'robot26.svg',
+  alt: 'robot26'
+}, {
+  file: 'robot25.svg',
+  alt: 'robot25'
+}, {
+  file: 'robot22.svg',
+  alt: 'robot22'
+}, {
+  file: 'robot17.svg',
+  alt: 'robot17'
+}, {
+  file: 'robot21.svg',
+  alt: 'robot21'
+}, {
+  file: 'rounded46.svg',
+  alt: 'rounded46'
+}];
 // The method below may be called on any array
 // to shuffle it in place.
 Array.prototype.shuffle = function () {
@@ -48,8 +40,9 @@ Array.prototype.shuffle = function () {
   if (i === 0) return this;
   while (--i) {
     j = Math.floor(Math.random() * (i + 1));
-    [this[i], this[j]] = [this[j], this[i]];
-
+    var _ref = [this[j], this[i]];
+    this[i] = _ref[0];
+    this[j] = _ref[1];
   }
   return this;
 };
@@ -69,38 +62,38 @@ function initGame() {
   // shuffle that deck
   deck.shuffle();
   // Assign an image (denoted by the index) to each card element
-  for(var j = 0; j < 2 * sources.length; j++){
+  for (var j = 0; j < 2 * sources.length; j++) {
     image['card_' + j] = deck[j];
   }
 }
 var flipped = null; // the last card flipped if still open
 var matched = 0; // number of pairs matched
 initGame();
-function flipCard(event){
-  if(event.target.className === 'card'){
-    if (event.target.alt === 'back'){
+function flipCard(event) {
+  if (event.target.className === 'card') {
+    if (event.target.alt === 'back') {
       var imageIndex = image[event.target.id];
       event.target.src = sources[imageIndex].file;
       event.target.alt = sources[imageIndex].alt;
-      if(!flipped){
+      if (!flipped) {
         flipped = event.target.id;
       } else {
-        if(image[flipped] === image[event.target.id]){
-        // there is a match
+        if (image[flipped] === image[event.target.id]) {
+          // there is a match
           vanish(flipped);
           vanish(event.target.id);
           flipped = null;
           matched++;
-      // if all cards have been matched
-          if(matched === sources.length){
+          // if all cards have been matched
+          if (matched === sources.length) {
             document.getElementById('board').className = 'win';
             document.getElementById('message').textContent = 'All matched!';
           }
         } else {
-        // disable flips for a second until cards are flipped back
+          // disable flips for a second until cards are flipped back
           document.getElementById('board').removeEventListener('click', flipCard, false);
-        // turn both cards back after a 1 sec delay
-          setTimeout(function(){
+          // turn both cards back after a 1 sec delay
+          setTimeout(function () {
             flipBack(flipped);
             flipped = null;
             flipBack(event.target.id);
@@ -111,20 +104,13 @@ function flipCard(event){
     }
   }
 }
-function flipBack(elementId){
+function flipBack(elementId) {
   document.getElementById(elementId).alt = 'back';
   document.getElementById(elementId).src = 'circuit4.svg';
 }
-function vanish(elementId){
+function vanish(elementId) {
   document.getElementById(elementId).className = 'matched';
 }
 // Register event handler
 
 document.getElementById('board').addEventListener('click', flipCard, false);
-
-
-
-
-
-
-
