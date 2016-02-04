27b53137ed69f2f731a7f8d56feb85e83c4282cc
“Klammertime” <audrey.ck@gmail.com>
2016-02-03 20:32:20 -0800

    Implement gulp-useref.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,22 +1,44 @@
 "use strict";
-var gulp = require('gulp'),
-    del = require('del'),
-    pages = require('gulp-gh-pages'),
-    rename = require('gulp-rename'),
-    fs = require('fs'),
-    path = require('path'),
-    traceur = require('gulp-traceur'),
-    babel = require('gulp-babel'),
-    plumber = require('gulp-plumber'),
-    es6Path = 'scripts/script.js',
-    compilePath = 'scripts/compiled';
+    var gulp = require('gulp'),
+         del = require('del'),
+       pages = require('gulp-gh-pages'),
+      rename = require('gulp-rename'),
+          fs = require('fs'),
+        path = require('path'),
+     traceur = require('gulp-traceur'),
+       babel = require('gulp-babel'),
+     plumber = require('gulp-plumber'),
+      useref = require('gulp-useref'),
+      uglify = require('gulp-uglify'),
+         iff = require('gulp-if'),
+        csso = require('gulp-csso'),
+     es6Path = 'src/scripts/script.js',
+ compilePath = 'src/scripts/compiled';
 
 var options = {
-    dist: './dist/'
+    dist: 'dist',
+    src: 'src'
 };
 
 
-/* babel test */
+
+var index = 1;
+
+gulp.task('renameImages', function() {
+    return gulp.src(options.src + '/img/*/*')
+        .pipe(rename(function(path) {
+            if (index > 10) {
+                index = 1;
+            }
+            path.basename = path.dirname + index++;
+        }))
+        .pipe(gulp.dest(options.src + '/img'));
+});
+
+gulp.task('watchFiles', function() {
+    gulp.watch([es6Path], ['traceur', 'babel']);
+});
+
 gulp.task('traceur', function() {
     return gulp.src([es6Path])
         .pipe(plumber())
@@ -33,45 +55,33 @@ gulp.task('babel', function() {
         .pipe(gulp.dest(compilePath + '/babel'));
 });
 
-gulp.task('watch', function() {
-    gulp.watch([es6Path], ['traceur', 'babel']);
-});
-
-
-
-/* end babel test */
-
-var index = 1;
-
-gulp.task('renameImages', function() {
-    return gulp.src('img/*/*')
-        .pipe(rename(function(path) {
-            if (index > 10) {
-                index = 1;
-            }
-            path.basename = path.dirname + index++;
-        }))
-        .pipe(gulp.dest('img'));
-});
-
 // Create clean task.
 gulp.task('clean', function() {
-    return del(['dist']);
+    return del([options.dist]);
 });
 
-gulp.task("build", ['renameImages'], function() {
+// Takes html file and runs through useref, tells html
+// what script and style files have based on index.html
+gulp.task('html', function() {
+    gulp.src(options.src + '/index.html')
+        // if files end in .js, apply uglify method
+        .pipe(iff('*.js', uglify()))
+        // if files end in .css, applu csso
+        .pipe(iff('*.css', csso()))
+        .pipe(useref())
+        .pipe(gulp.dest(options.dist));
+});
+
+gulp.task('build', ['renameImages', 'html'], function() {
     return gulp.src([
-            "index.html",
-            "memory.appcache",
-            "styles/*",
-            "img/*/*",
-            "scripts/*/*/*",
-            "scripts/*",
-            "img/*.svg"
+            'index.html',
+            options.src + '/memory.appcache',
+            options.src + '/img/*/*',
+            options.src + '/img/*.svg'
         ], {
-            base: "./"
+            base: options.src
         })
-        .pipe(gulp.dest("dist"));
+        .pipe(gulp.dest(options.dist));
 });
 
 gulp.task('deploy', function() {
@@ -79,7 +89,9 @@ gulp.task('deploy', function() {
         .pipe(pages());
 });
 
+gulp.task('serve', ['watchFiles']);
+
 // Build task is a dependency of default task so can run command "gulp".
-gulp.task("default", ["clean", "traceur", "babel", "watch"], function() {
+gulp.task('default', ['clean', 'traceur', 'babel'], function() {
     gulp.start('build');
 });
